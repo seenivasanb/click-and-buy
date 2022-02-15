87ad34c6dc81da6b363554d8063d404ec5505138
Seenivasan Balakrishan <seenivasan.balakrishnan@csscorp.com>
2022-02-16 01:35:16 +0530

    'Responsive

--- src/components/product-list-component/index.tsx
+++ src/components/product-list-component/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { ProductCategoryType, ProductSizeType, ProductType } from "types/products";
+import { ProductAlignmentType, ProductCategoryType, ProductSizeType, ProductType } from "../../types/products";
 import { ProductComponent } from "../../components";
 import "./index.css"
 
 type Props = {
     category: ProductCategoryType,
-    noOfColumns?: number,
+    alignment?: ProductAlignmentType,
     size?: ProductSizeType,
     products: ProductType[]
 }
@@ -13,17 +13,17 @@ type Props = {
 /**
  * Base Template component holding the HashRouter along with all the basic routes
  * @param category - specifies the type of the product that will be [productWithCTA, productWithSize, productTwoColumns]
- * @param noOfColumns - specifies the no of columns that the product should be display
+ * @param alignment - specifies the alignment of the products should be display that will be [row, column]
  * @param size - specifies the size of the product that will be [small, medium, large]
  * @param products - array of objects that contains the list of products details
  * @returns {JSX.Element}
  */
-export default ({ category, noOfColumns, size, products }: Props) => {
+export default ({ category, alignment = ProductAlignmentType.row, size, products }: Props): JSX.Element => {
     console.log("Product List Component");
 
     return (
-        <section className="products" style={{ gridTemplateColumns: `repeat(${noOfColumns}, minmax(0, 1fr))` }}>
-            {products?.slice(0, noOfColumns)?.map(product =>
+        <section className={`products flex-${alignment}`} style={{ flexDirection: alignment === "row" ? "row" : "column" }}>
+            {products?.map(product =>
                 <ProductComponent key={product.id} category={category} size={size} product={product} />
             )}
         </section>
